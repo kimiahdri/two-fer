767325dbb6ddc83d3ed12cdd128c12a6956538bb
kimi <kimi@DESKTOP-HLSO57E>
2022-07-18 00:36:19 +0430

    Add project files.

---
+++ two fer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace two_fer
+{
+    class Program
+    {
+        static void string speak()
+        {
+            return "one for you one for me.";
+        }
+
+        static void string speak(string h)
+        {
+            if (h == "Bob")
+            {
+                return "one for bob one for me.";
+            }
+            else
+            {
+                return "one for alice one for me.";
+            }
+        }
+    }
+}
